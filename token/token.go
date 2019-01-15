db9dee23a903ca8bc2b0b9c31d28c614f25abcd9
leica19 <spike-19@ezweb.ne.jp>
2019-01-15 17:10:38 +0900

    f com

---
+++ token/token.go
@@ -0,0 +1,35 @@
+
+package token
+
+type TokenType string
+
+type Token struct {
+  Type    TokenType
+  Literal string
+}
+
+const (
+  ILLEGAL = "ILLEGAL"
+  EOF = "EOF"
+  
+  //
+  IDENT = "IDENT"
+  INT = "INT"
+  
+  //
+  ASSIGN = "="
+  PLUS = "+"
+  
+  // 
+  COMMA = ","
+  SEMICOLON = ";"
+  
+  LPAREN = "("
+  RPAREN = ")"
+  LBRACE = "{"
+  RBRACE = "}"
+  
+  // key words
+  FUNCTION = "FUNCTION"
+  LET = "LET"
+)
